7a7ce56570287631698eea8194af4d4db2628757
rahul naik <rahulnaik6241@gmail.com>
2025-01-22 13:54:08 +0530

    Cleaned up repository - removed unnecessary files

--- config.example.js
+++
@@ -1,6 +0,0 @@
-// Rename this file to config.js and add your API key
-const config = {
-    apiKey: process.env.WEATHER_API_KEY || "YOUR_API_KEY_HERE"
-};
-
-export default config;
---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiKey: "fa96bea40e1bbdb6b5f04fd827778081"
+};
+
+export default config;
